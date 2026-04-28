ac96ffb8a5e881b6a31e952705fa99a8d15b762a
nithin594844-gif <nithin.5948.44@gmail.com>
2026-04-28 19:30:54 +0530

    UC7: Addition with Target Unit Specification

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -33,11 +33,19 @@ public class QuantityMeasurementApp {
             return new Length(round(result), targetUnit);
         }
         public Length add(Length other) {
-            if (other == null)
-                throw new IllegalArgumentException("Length cannot be null");
             double sumInches = this.toInches() + other.toInches();
             double result = fromInches(sumInches, this.unit);
             return new Length(round(result), this.unit);
+        }
+        public Length add(Length other, LengthUnit targetUnit) {
+            return addAndConvert(other, targetUnit);
+        }
+        private Length addAndConvert(Length other, LengthUnit targetUnit) {
+            if (other == null || targetUnit == null)
+                throw new IllegalArgumentException("Invalid input");
+            double sumInches = this.toInches() + other.toInches();
+            double result = fromInches(sumInches, targetUnit);
+            return new Length(round(result), targetUnit);
         }
         @Override
         public boolean equals(Object obj) {
@@ -49,40 +57,55 @@ public class QuantityMeasurementApp {
             return Double.compare(this.toInches(), other.toInches()) == 0;
         }
         private double round(double val) {
-            return Math.round(val * 100.0) / 100.0;
+            return Math.round(val * 1000.0) / 1000.0; // more precision
         }
         @Override
         public String toString() {
             return "Quantity(" + value + ", " + unit + ")";
         }
     }
-    public static Length add(Length l1, Length l2) {
-        return l1.add(l2);
+    public static Length add(Length l1, Length l2, Length.LengthUnit target) {
+        return l1.add(l2, target);
     }
     public static void main(String[] args) {
-        System.out.println("Input: add(Quantity(1.0, FEET), Quantity(2.0, FEET))");
+        System.out.println("\nInput: add(Quantity(1.0, FEET), Quantity(12.0, INCHES), FEET)");
         System.out.println("Output: " + add(new Length(1.0, Length.LengthUnit.FEET),
-                new Length(2.0, Length.LengthUnit.FEET)));
-        System.out.println("\nInput: add(Quantity(1.0, FEET), Quantity(12.0, INCHES))");
+                new Length(12.0, Length.LengthUnit.INCHES),
+                Length.LengthUnit.FEET));
+
+        System.out.println("\nInput: add(Quantity(1.0, FEET), Quantity(12.0, INCHES), INCHES)");
         System.out.println("Output: " + add(new Length(1.0, Length.LengthUnit.FEET),
-                new Length(12.0, Length.LengthUnit.INCHES)));
-        System.out.println("\nInput: add(Quantity(12.0, INCHES), Quantity(1.0, FEET))");
-        System.out.println("Output: " + add(new Length(12.0, Length.LengthUnit.INCHES),
-                new Length(1.0, Length.LengthUnit.FEET)));
-        System.out.println("\nInput: add(Quantity(1.0, YARDS), Quantity(3.0, FEET))");
+                new Length(12.0, Length.LengthUnit.INCHES),
+                Length.LengthUnit.INCHES));
+
+        System.out.println("\nInput: add(Quantity(1.0, FEET), Quantity(12.0, INCHES), YARDS)");
+        System.out.println("Output: " + add(new Length(1.0, Length.LengthUnit.FEET),
+                new Length(12.0, Length.LengthUnit.INCHES),
+                Length.LengthUnit.YARDS));
+
+        System.out.println("\nInput: add(Quantity(1.0, YARDS), Quantity(3.0, FEET), YARDS)");
         System.out.println("Output: " + add(new Length(1.0, Length.LengthUnit.YARDS),
-                new Length(3.0, Length.LengthUnit.FEET)));
-        System.out.println("\nInput: add(Quantity(36.0, INCHES), Quantity(1.0, YARDS))");
+                new Length(3.0, Length.LengthUnit.FEET),
+                Length.LengthUnit.YARDS));
+
+        System.out.println("\nInput: add(Quantity(36.0, INCHES), Quantity(1.0, YARDS), FEET)");
         System.out.println("Output: " + add(new Length(36.0, Length.LengthUnit.INCHES),
-                new Length(1.0, Length.LengthUnit.YARDS)));
-        System.out.println("\nInput: add(Quantity(2.54, CENTIMETERS), Quantity(1.0, INCHES))");
+                new Length(1.0, Length.LengthUnit.YARDS),
+                Length.LengthUnit.FEET));
+
+        System.out.println("\nInput: add(Quantity(2.54, CENTIMETERS), Quantity(1.0, INCHES), CENTIMETERS)");
         System.out.println("Output: " + add(new Length(2.54, Length.LengthUnit.CENTIMETERS),
-                new Length(1.0, Length.LengthUnit.INCHES)));
-        System.out.println("\nInput: add(Quantity(5.0, FEET), Quantity(0.0, INCHES))");
+                new Length(1.0, Length.LengthUnit.INCHES),
+                Length.LengthUnit.CENTIMETERS));
+
+        System.out.println("\nInput: add(Quantity(5.0, FEET), Quantity(0.0, INCHES), YARDS)");
         System.out.println("Output: " + add(new Length(5.0, Length.LengthUnit.FEET),
-                new Length(0.0, Length.LengthUnit.INCHES)));
-        System.out.println("\nInput: add(Quantity(5.0, FEET), Quantity(-2.0, FEET))");
+                new Length(0.0, Length.LengthUnit.INCHES),
+                Length.LengthUnit.YARDS));
+
+        System.out.println("\nInput: add(Quantity(5.0, FEET), Quantity(-2.0, FEET), INCHES)");
         System.out.println("Output: " + add(new Length(5.0, Length.LengthUnit.FEET),
-                new Length(-2.0, Length.LengthUnit.FEET)));
+                new Length(-2.0, Length.LengthUnit.FEET),
+                Length.LengthUnit.INCHES));
     }
 }
