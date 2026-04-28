16a85abf5acd6b597b259ac1284a896f2c96b53f
nithin594844-gif <nithin.5948.44@gmail.com>
2026-04-28 19:10:30 +0530

    UC3: Generic Quantity Class for DRY Principle

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,60 +1,63 @@
 public class QuantityMeasurementApp {
-    public static class Feet {
-        private final double value;
-        public Feet(double value) {
+    static class Length {
+        private double value;
+        private LengthUnit unit;
+        public enum LengthUnit {
+            FEET(12.0),
+            INCHES(1.0);
+            private final double conversionFactor;
+            LengthUnit(double conversionFactor) {
+                this.conversionFactor = conversionFactor;
+            }
+            public double getConversionFactor() {
+                return conversionFactor;
+            }
+        }
+        public Length(double value, LengthUnit unit) {
             this.value = value;
+            this.unit = unit;
+        }
+        private double convertToBaseUnit() {
+            return value * unit.getConversionFactor();
+        }
+        public boolean compare(Length that) {
+            return Double.compare(this.convertToBaseUnit(),
+                    that.convertToBaseUnit()) == 0;
         }
         @Override
-        public boolean equals(Object obj) {
-            if(this == obj)
+        public boolean equals(Object o) {
+            if (this == o)
                 return true;
-            if(obj == null)
+            if (o == null || getClass() != o.getClass())
                 return false;
-            if(getClass() != obj.getClass())
-                return false;
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
+            Length that = (Length) o;
+            return compare(that);
         }
     }
-    public static class Inches {
-        private final double value;
-        public Inches(double value) {
-            this.value = value;
-        }
-        @Override
-        public boolean equals(Object obj) {
-            if(this == obj)
-                return true;
-            if(obj == null)
-                return false;
-            if(getClass() != obj.getClass())
-                return false;
-            Inches other = (Inches) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
+    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+        return l1.equals(l2);
     }
     public static void demonstrateFeetEquality() {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+        Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length l2 = new Length(1.0, Length.LengthUnit.FEET);
         System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal(" + f1.equals(f2) + ")");
+        System.out.println("Output: Equal (" + demonstrateLengthEquality(l1, l2) + ")");
     }
     public static void demonstrateInchesEquality() {
-        Inches i1 = new Inches(1.0);
-        Inches i2 = new Inches(1.0);
+        Length l1 = new Length(1.0, Length.LengthUnit.INCHES);
+        Length l2 = new Length(1.0, Length.LengthUnit.INCHES);
         System.out.println("Input: 1.0 inch and 1.0 inch");
-        System.out.println("Output: Equal(" + i1.equals(i2) + ")");
+        System.out.println("Output: Equal (" + demonstrateLengthEquality(l1, l2) + ")");
+    }
+    public static void demonstrateFeetInchesComparison() {
+        Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length l2 = new Length(12.0, Length.LengthUnit.INCHES);
+        System.out.println("Input: Quantity(1.0, \"feet\") and Quantity(12.0, \"inches\")");
+        System.out.println("Output: Equal (" + demonstrateLengthEquality(l1, l2) + ")");
     }
     public static void main(String[] args) {
-        demonstrateFeetEquality();;
+        demonstrateFeetEquality();
         demonstrateInchesEquality();
+        demonstrateFeetInchesComparison();
     }
 }
