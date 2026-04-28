7ada07264e370df9162e89d53722101aaba5c727
nithin594844-gif <nithin.5948.44@gmail.com>
2026-04-28 18:47:19 +0530

    UC2: Feet and Inches measurement equality

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -15,11 +15,46 @@ public class QuantityMeasurementApp {
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
         }
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+    public static class Inches {
+        private final double value;
+        public Inches(double value) {
+            this.value = value;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if(this == obj)
+                return true;
+            if(obj == null)
+                return false;
+            if(getClass() != obj.getClass())
+                return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+    public static void demonstrateFeetEquality() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal(" + f1.equals(f2) + ")");
+    }
+    public static void demonstrateInchesEquality() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal(" + i1.equals(i2) + ")");
     }
     public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(10);
-        System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + f1.equals(f2) + ")");
+        demonstrateFeetEquality();;
+        demonstrateInchesEquality();
     }
 }
